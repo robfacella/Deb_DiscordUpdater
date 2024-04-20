f4aaf8c81e910060c52b8a7bd9adc1c26b45a5b4
BuildTools <rob.facella@gmail.com>
2024-04-20 12:04:57 -0400

    Get most recent Stable dotDeb

---
+++ UpdateDiscord.sh
@@ -0,0 +1,8 @@
+# Get the most recent Stable Release
+wget https://discord.com/api/download/stable?platform=linux&format=deb
+
+# Pause for DL
+sleep 10s
+
+# Rename it to a dot deb
+mv stable?platform=linux discord.deb
