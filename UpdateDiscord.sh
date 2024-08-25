528df0672683eea7d8d4876b369e980565e8a0eb
robfacella <rob.facella@gmail.com>
2024-08-25 11:14:04 -0400

    Delay until wget download completes

--- UpdateDiscord.sh
+++ UpdateDiscord.sh
@@ -4,13 +4,14 @@
 wget https://discord.com/api/download/stable?platform=linux&format=deb
 
 # Debug Msg
-echo "\nDownloaded..\n"
-
-# Pause for DL, mightt be wholly unecessary
+echo "Downloaded.."
+# Currently Prints before wget's download completes,
+# Pause for DL, might be wholly unecessary
 sleep 5s
+# if there's a way to freeze execution specifically until wget completes its download
 
 # Debug Msg
-echo "\nInstalling..\n"
+echo "Installing.."
 
 # Rename it to a dot deb
 mv stable?platform=linux discord.deb
