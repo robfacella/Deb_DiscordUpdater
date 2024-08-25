7e04fddd6e932e9998aec4badb6ba2038a03752b
robfacella <rob.facella@gmail.com>
2024-08-25 11:07:48 -0400

    Needs root

---
+++ Total.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#pwd
+#ls
+
+# Freezes after User Change until Exit, then continues without permissions
+#su - && apt update && apt upgrade -y
+
+apt update && apt upgrade -y
+
+#ls
+#pwd
+bash UpdateDiscord.sh
