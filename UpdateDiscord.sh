2e1038bb7ed4782b32419be4ae3ee1dd6f8ed063
robfacella <rob.facella@gmail.com>
2024-10-26 09:42:44 -0400

    golf

--- UpdateDiscord.sh
+++ UpdateDiscord.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-
 # Get the most recent Stable Release
 wget "https://discord.com/api/download/stable?platform=linux&format=deb"
-
 # Debug Msg
-echo "Downloaded.."
-# Sleep not needed. Just needed to enclose file and URL pathing in quotes; the '&' was being taken literally
-
-# Debug Msg
-echo "Installing.."
-
+echo "Downloaded! Installing... "
+# Sleep not needed. Just needed to enclose file and URL pathing in quotes; the '&' in URL was being taken literally
 # Rename it to a dot deb. Same, enclose with quotes for '&' sanity
 mv "stable?platform=linux&format=deb" "discord.deb"
-
 # Install, will require root or sudo
 dpkg -i discord.deb
